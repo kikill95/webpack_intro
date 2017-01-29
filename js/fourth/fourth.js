a45e596ea915def04ce6f2152f3009c46aec0c9a
light <kirill.gus@gmail.com>
2017-01-29 23:42:53 +0200

    Add more example code for generators and for async-await

--- js/fourth/fourth.js
+++ js/fourth/fourth.js
@@ -3,25 +3,37 @@ import $ from 'jquery';
 export default function() {
     // es7
     // async - await
+    // Nice example: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+
     // promises. Example: https://github.com/kikill95/nodejs-servers/blob/master/express/index.html
     function getRandomPonyFooArticle() {
-        return new Promise((resolve, reject) => {
-            $.get('http://api.alice.com/cors', (res, body) => {
-                resolve(body);
-            }).catch(err => {
-                if (err) {
-                    // reject(err);
-                    resolve('Tested'); // we resolved for example
-                    return;
-                }
-            });
+      return new Promise((resolve, reject) => {
+        $.get('https://111crossorigin.me/https://google.com', (res, status) => {
+          resolve(status);
+        }).catch(err => {
+          if (err) {
+            reject(err);
+          }
         });
+      });
     }
 
-    read();
+    read()
+      .then(function(result) {
+        console.log('then', result);
+      })
+      .catch(function(err) {
+        console.log('err', err);
+      });
 
     async function read() {
+      // try {
         var result = await getRandomPonyFooArticle();
-        // console.log(result);
+      // } catch (err) {
+      //   console.log('err in the catch', err);
+      //   // in this way we can handle every await one by one
+      // }
+      console.log('after await', result);
+      return result;
     }
 }
--- js/third/third.js
+++ js/third/third.js
@@ -1,15 +1,36 @@
 module.exports = () => {
   // generators
-  function* idMaker() {
+  function* idMaker(){
     var index = 0;
-    console.log('invider');
-    while(true)
+    while(true) {
       yield index++;
+    }
+  }
+
+  var gen = idMaker();
+
+  // console.log(gen.next().value); // 0
+  // console.log(gen.next().value); // 1
+  // console.log(gen.next().value); // 2
+
+  // generators advanced
+  function* idMaker() {
+    var index = 0,
+      savedValue;
+    console.log('inside');
+    while(true) {
+      index++;
+      savedValue = yield index;
+      console.log('savedValue', savedValue);
+      if (savedValue) {
+        index = 0;
+      }
+    }
   }
 
   var gen = idMaker();
 
   // console.log(gen.next());
   // console.log(gen.next());
-  // console.log(gen.next());
+  // console.log(gen.next(true));
 };
